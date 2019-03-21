9cadb3fa507b1ff3295230354c466a8f2e1e75a0
Niels Söth <nielssoeth@gmx.de>
2019-03-21 10:23:27 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = require('./main');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = [
   new HtmlWebpackPlugin({
@@ -10,5 +11,9 @@ module.exports = [
   new CopyWebpackPlugin([
     // { from: 'src/assets', to: './assets' }
   ]),
+  new WebpackShellPlugin({
+    onBuildStart: ['node ./config/typedCssModules.js'],
+    dev: false
+  }),
   new ForkTsCheckerWebpackPlugin()
 ];
